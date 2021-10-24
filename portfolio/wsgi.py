aa8fba994368ab53d23c3d6b7256a1ea46401e6c
Hamza sabib <hsabib@protonmail.com>
2021-10-24 22:41:02 +0100

    add procfile and runtime

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.developement')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.production')
 
 application = get_wsgi_application()
